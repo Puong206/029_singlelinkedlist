7b2c635a24b7133b4090511d6d542d7669485420
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2024-04-25 17:31:23 +0700

    void add node

--- Singlelinkedlist/Singlelinkedlist.cpp
+++ Singlelinkedlist/Singlelinkedlist.cpp
@@ -4,11 +4,48 @@ using namespace std;
 struct Node {
 	int noMhs;
 	string name;
-	Node* text;
+	Node* next;
 };
 
 Node* START = NULL;
 
 void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukkan NIM : ";
+	cin >> nim;
+	cout << "Masukkan Nama : ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
 
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+
+	Node* previous = START;
+	START = nodeBaru;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
+
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
 }
